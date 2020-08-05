339fdeab0efb10e90a161418db987b3a2bd781bb
Maria Ferdinand <Maria Ferdinand@10.0.0.116>
2020-08-05 13:18:27 -0400

    Add in more files

---
+++ TicTacToe/src/main/java/LogiicalOperator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria Ferdinand
+ */
+public class LogiicalOperator {
+    public static void main(String []args){
+           int number1=1, number2=2,number3=9;
+           boolean result;
+           result = (number1>number2)|| (number3>number1);
+           
+           
+    }
+}
