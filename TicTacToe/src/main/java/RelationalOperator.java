d40a855b04c34bb4c6dd87a2f106109b96892060
Maria Ferdinand <Maria Ferdinand@10.0.0.116>
2020-08-05 12:00:01 -0400

    Add in more files

---
+++ TicTacToe/src/main/java/RelationalOperator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria Ferdinand
+ */
+public class RelationalOperator {
+    public static void main (String[] args){ 
+       int num1 =9;
+       int num2 = 45;
+      
+      
+       
+       
+       if (num1>num2){  
+          System.out.println("number1 is greater than number 2"); 
+       }    
+       
+       else 
+       {
+          System.out.println("number 2 is greater than number 1");
+       }
+       
+       
+       
+       }
+    }
