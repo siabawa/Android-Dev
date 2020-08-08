71e6e3d9e1b7903829416dcc27184a8a9c0ba1dd
Maria Ferdinand <Maria Ferdinand@10.0.0.116>
2020-08-07 23:50:10 -0400

    Add in more files

---
+++ TicTacToe/src/main/java/IfElse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria Ferdinand
+ */
+public class IfElse {
+    public static void main(String [] args){
+        int number = 10;   //specify your data type
+        int test = 0;
+        if(test < 10)
+        {  
+        // body of if
+     System.out.println("This number is postive"); 
+        }
+    
+       else{
+            
+            System.out.println("Thus number is negative");
+               
+        } 
+}
+}
