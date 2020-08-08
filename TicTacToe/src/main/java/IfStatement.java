8b41805c9999a735bf1f0b928cb614e58c095f22
Maria Ferdinand <Maria Ferdinand@10.0.0.116>
2020-08-07 23:51:10 -0400

    Add in more files

---
+++ TicTacToe/src/main/java/IfStatement.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria Ferdinand
+ */
+public class IfStatement {
+    public static void main(String[] args){
+        int number = 10;
+        
+    // checks if number is greater than 0
+    if (number>0) {
+     System.out.println("The number is positive.");
+    }
+    System.out.println("This statement is always executed.");
+  }
+  
+}
