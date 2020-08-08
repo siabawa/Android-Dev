0c2d7012d51455335be893462a4c71b43b0f03ac
Maria Ferdinand <Maria Ferdinand@10.0.0.116>
2020-08-07 23:52:35 -0400

    Add in more files

---
+++ TicTacToe/src/main/java/Ladder.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria Ferdinand
+ */
+public class Ladder {
+    public static void main(String [] args) {
+        int num = 0 ;
+          if (num > 0){ 
+          
+          System.out.println("number is postive"+ num);
+          }
+          else if (num < 0){
+          System.out.println("number is negative" + num);
+          }
+          else {
+              System.out.println("the number is zero");
+          }
+    }
+}
+//im so happy youre still practicing <3 youre doing amazing - btw i will be out but omgg warms my heart!(: - you can continue working now haha - ok byeeee!!  p.s dont forget to save ok thank you
