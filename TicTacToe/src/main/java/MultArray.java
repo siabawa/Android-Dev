a6d02ad04abe271f280e0e4a640d591fd93cd5df
Maria Ferdinand <Maria Ferdinand@10.0.0.116>
2020-08-07 23:53:05 -0400

    Add in more files

---
+++ TicTacToe/src/main/java/MultArray.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria Ferdinand
+ */
+public class MultArray {
+    public static void main(String[] args){
+        int[][] c = {
+            {1,2,3}, 
+            {4,5,6,9}, 
+            {7},
+        };
+            System.out.println("Length is " + c[0].length);
+}
+}
