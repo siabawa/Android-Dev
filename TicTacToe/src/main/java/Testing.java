bebc3b4ed423694030b78d5f6640098e50b5cc2f
Maria Ferdinand <Maria Ferdinand@10.0.0.116>
2020-08-07 23:58:54 -0400

    Add in more files

---
+++ TicTacToe/src/main/java/Testing.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria Ferdinand
+ */
+public class Testing {
+    public static void main(String[] args){
+        String [] cars = {"Volvo", "BMW", "Ford","Mazda"};
+        cars [0] = "Opel";
+        System.out.println("second index is " + cars [2]);
+    }
+}
